867d49d3b6028406205a036e833359860ff2d67e
Ajay Susanth <ajaysusanth10@gmail.com>
2024-09-15 21:10:20 +0530

    verification-email

---
+++ types/apiResponse.ts
@@ -0,0 +1,7 @@
+import { Message } from "@/models/User";
+export interface apiResponse {
+    success:boolean;
+    message:string;
+    isAcceptingMessages ?: boolean;
+    messages ?: Array<Message>;     
+}
